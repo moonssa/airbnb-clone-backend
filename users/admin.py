ceaa93b2aaadb41c950630722fef274d57b83430
rebootedCoder2 <oborimoon@gmail.com>
2024-04-22 10:37:01 +0900

    room model

--- users/admin.py
+++ users/admin.py
@@ -2,27 +2,44 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
 
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                    "is_host",
+                    "gender",
+                    "language",
+                    "currency",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important dates",
+            {"fields": ("last_login", "date_joined"), "classes": ("collapse",)},
+        ),
+    )
 
-  fieldsets=(
-    ("Profile", 
-     {
-       "fields":("username", "password", "name", "email", "is_host")
-      },),
-    ("Permissions",
-     {
-          "fields": (
-              "is_active",
-              "is_staff",
-              "is_superuser",
-              "groups",
-              "user_permissions",
-          ),
-          "classes":("collapse",),
-      },),
-      ("Important dates", {"fields": ("last_login", "date_joined"), "classes":("collapse",)}),
-  )
-
-  list_display= ("username", "email", "name", "is_host")
+    list_display = ("username", "email", "name", "is_host")
