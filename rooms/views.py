e5f8a9ba774f36e49688259ce744f035dcc8750f
rebootedCoder2 <oborimoon@gmail.com>
2024-04-24 16:02:46 +0900

    url argument

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def see_all_rooms(request):
+    return HttpResponse("See all Rooms")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"See one room : {room_id}")
