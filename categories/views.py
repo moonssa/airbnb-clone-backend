a6d435d18ea1e8b22734c6af6c1d371ee1cfed65
rebootedCoder2 <oborimoon@gmail.com>
2024-05-02 09:03:22 +0900

    DJango rest framework - APIView

--- categories/views.py
+++ categories/views.py
@@ -4,21 +4,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
-
+class Categories(APIView):
+    def get(self, request):
         all_categoriees = Category.objects.all()
         serializer = CategorySerializer(all_categoriees, many=True)
         return Response(
             serializer.data,
         )
-    elif request.method == "POST":
-        # json 형태로 입력받은  data => QuerySet 형태로 변환
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
@@ -30,7 +29,36 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT", "DELETE"])
+class CategoryDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+""" @api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -52,4 +80,4 @@ def category(request, pk):
             return Response(serializer.errors)
     elif request.method == "DELETE":
         category.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_204_NO_CONTENT) """
